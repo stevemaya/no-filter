7e628937ab5efe97ad19fda8439ae9233d8be281
Steve Maya <steve.maya@codeimmersives.com>
2020-10-13 11:35:56 -0500

    added onlyEvens

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
  * YOUR CODE HERE *
  ******************/
 
+function onlyOdds(nums) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+      if(nums[i] % 2 === 1) {
+          result.push(nums[i]);
+      }
+  }
+  return result;
+}
+
+
+function onlyEvens(nums) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+      if(nums[i] % 2 === 0) {
+          result.push(nums[i]);
+      }
+  }
+  return result;
+}
+
 
 
 
